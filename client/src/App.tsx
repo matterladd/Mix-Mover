315ed9a3058f98fad732fda0f482790e3351d38c
matterladd <michael.cook972@gmail.com>
2026-05-19 17:52:09 -0400

    I like them semicolons, so I added them

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
   return (
     <>
       <p>poopcock</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
