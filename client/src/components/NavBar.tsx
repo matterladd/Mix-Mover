cc5f68f459a3f71d092387c70f7523102035bf7d
matterladd <michael.cook972@gmail.com>
2026-06-02 10:47:42 -0400

    improvements to navbar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom'
 export default function NavBar() {
     return (
         <>
-            <Link to="/">Go to Home</Link>
-            <span> </span>
+            <Link to="/">Home</Link>
+            <span> | </span>
             <Link to="/login">Login</Link>
-            <span> </span>            
+            <span> | </span>            
             <Link to="/signup">Sign up</Link>
-            <span> </span>
+            <span> | </span>
             <a href='/auth'>Authorize your account</a>
-            <span> </span>
-            <Link to="Dashboard">Go to Dashboard</Link>
+            <span> | </span>
+            <Link to="dashboard">Dashboard</Link>
         </>
     );
 }
