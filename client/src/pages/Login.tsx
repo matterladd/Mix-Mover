d5957f1239bc0c38487b07255ee858c8ebc6df55
matterladd <michael.cook972@gmail.com>
2026-06-18 12:24:33 -0400

    add shadcn LoginForm base

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "@/context/AuthContext";
+import { LoginForm } from "@/components/login-form";
 
 export default function Login() {
     const navigate = useNavigate();
@@ -39,6 +40,11 @@ export default function Login() {
                 <input value={password} onChange={(e) => setPassword(e.target.value)} />
                 <button type="submit">Login</button> {/* type is not necessary here but added for clarity */}
             </form>
+            <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+                <div className="w-full max-w-sm">
+                    <LoginForm />
+                </div>
+            </div>
         </>
     );
 }
