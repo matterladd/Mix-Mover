9219ef32f3975a722e67d1f2d14ef7f17cd5c58c
matterladd <michael.cook972@gmail.com>
2026-06-18 16:41:13 -0400

    implement shadcn login form functionality

--- client/src/components/login-form.tsx
+++ client/src/components/login-form.tsx
@@ -14,12 +14,40 @@ import {
   FieldLabel,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
-import { Link } from 'react-router-dom'
+import { useAuthContext } from "@/context/AuthContext";
+import { useState } from "react"
+import { Link, useNavigate } from 'react-router-dom'
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState(""); // TODO: probably unsafe storing this as a state?
+  const [isLoggedIn, setIsLoggedIn] = useState(true); // set for first time display
+  const { setUser } = useAuthContext();
+
+  // declared inside LoginForm component because it needs access to component state
+  async function handleSubmit(e: React.SubmitEvent) {
+    e.preventDefault(); // stops the default form submit behavior (sending http)
+
+    const res = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include', // tells browser to send/receive cookies
+        body: JSON.stringify({ email, password })
+    });
+    if (!res.ok) {
+        const data = await res.json();
+        console.error(data.error);
+        setIsLoggedIn(false);
+    } else {
+        setUser({id: null, email: email});
+        navigate('/');
+    }
+  }
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -30,7 +58,7 @@ export function LoginForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={handleSubmit}>
             <FieldGroup>
               <Field>
                 <FieldLabel htmlFor="email">Email</FieldLabel>
@@ -38,6 +66,8 @@ export function LoginForm({
                   id="email"
                   type="email"
                   placeholder="m@example.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   required
                 />
               </Field>
@@ -51,13 +81,15 @@ export function LoginForm({
                     Forgot your password?
                   </a>
                 </div>
-                <Input id="password" type="password" required />
+                <Input 
+                  id="password" 
+                  type="password" 
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required />
               </Field>
               <Field>
                 <Button type="submit">Login</Button>
-                <Button variant="outline" type="button">
-                  Login with Google
-                </Button>
                 <FieldDescription className="text-center">
                   Don't have an account? <Link to="/signup">Sign up</Link>
                 </FieldDescription>
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,45 +1,8 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useAuthContext } from "@/context/AuthContext";
 import { LoginForm } from "@/components/login-form";
 
 export default function Login() {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState(""); // TODO: probably unsafe storing this as a state?
-    const [isLoggedIn, setIsLoggedIn] = useState(true); // set for first time display
-    const { setUser } = useAuthContext();
-    
-    const handleSubmit = async (e: React.SubmitEvent) => {
-        e.preventDefault(); // stops the default form submit behavior (sending http)
-
-        const res = await fetch('/api/auth/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            credentials: 'include', // tells browser to send/receive cookies
-            body: JSON.stringify({ email, password })
-        });
-        if (!res.ok) {
-            const data = await res.json();
-            console.error(data.error);
-            setIsLoggedIn(false);
-        } else {
-            setUser({id: null, email: email});
-            navigate('/');
-        }
-    }
-
     return (
         <>
-            <h3>Login</h3>
-            <form onSubmit={handleSubmit}>
-                {!isLoggedIn && <p>Sorry, try again!</p>} 
-                <label>email: </label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} />
-                <label>password: </label>
-                <input value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit">Login</button> {/* type is not necessary here but added for clarity */}
-            </form>
             <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
                 <div className="w-full max-w-sm">
                     <LoginForm />
