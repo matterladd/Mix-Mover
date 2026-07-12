fcef7e711e9e5a88666b7902f95c70b4e5d66564
matterladd <michael.cook972@gmail.com>
2026-07-12 14:12:45 -0400

    add sheet.tsx for responsive nav design

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,18 +1,29 @@
 import { Link } from 'react-router-dom';
 import { 
-    NavigationMenu,
-    NavigationMenuList,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    navigationMenuTriggerStyle
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  navigationMenuTriggerStyle
 } from '@/components/ui/navigation-menu';
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Button } from './ui/button';
 import ThemeToggle from '@/components/theme-toggle';
 
 export default function NavBar() {
 
 return (
   <>
-    <NavigationMenu>
+    <NavigationMenu className={"hidden sm:flex"}>
       <NavigationMenuList>
         <NavigationMenuItem>
           <NavigationMenuLink 
@@ -52,6 +63,20 @@ return (
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
+    <div className='sm:hidden'>
+      <Sheet>
+        <SheetTrigger 
+          render={<Button variant={'ghost'}>Open</Button>}
+        >
+        </SheetTrigger>
+        <SheetContent side='left' className={'w-3/4 max-w-none sm:max-w-none min-w-0'}>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>This action cannot be undone.</SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </div>
   </>
 );
 }
