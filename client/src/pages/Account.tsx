8a5c67787805c1d96f66e849779786177b499af1
matterladd <michael.cook972@gmail.com>
2026-08-04 14:39:55 -0400

    fix linting error for Account page by moving each sub-component to be declared outside the main Account component

--- client/src/pages/Account.tsx
+++ client/src/pages/Account.tsx
@@ -13,19 +13,10 @@ import {
 import { useSpotifyContext } from "@/context/spotify/index";
 import { useEffect } from "react";
 
-export default function Account() {
+function AccountView() {
   const { user, setUser } = useAuthContext();
-  const { spotifyUser, setSpotifyUser, updateSpotifyUser } =
-    useSpotifyContext();
+  const { spotifyUser, setSpotifyUser } = useSpotifyContext();
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    if (params.get("spotify_connected") === "true") {
-      console.log("poop");
-      updateSpotifyUser();
-    }
-  }, []);
 
   function connectSpotifyAccount() {
     window.location.assign("/api/spotify_auth");
@@ -49,88 +40,97 @@ export default function Account() {
       console.error(err);
     }
   }
-
-  function GuestView() {
-    return (
-      <div className="flex w-full justify-center">
-        <div className="flex flex-col w-full max-w-md items-center">
-          <p className="font-extrabold text-4xl">not logged in</p>
+  return (
+    <div className="flex w-full justify-center">
+      <div className="flex flex-col w-full max-w-md items-center">
+        <h1 className="text-5xl font-bold pb-4">Account</h1>
+        <h3 className="text-2xl font-semibold pb-4">{user!.email}</h3>
+        <div className="flex pb-4">
+          <Button
+            className="max-w-fit"
+            variant="destructive"
+            onClick={handleLogout}
+          >
+            Logout
+          </Button>
+          <Button
+            className="max-w-fit"
+            variant="destructive"
+            onClick={() => alert("not implemented")}
+          >
+            Delete Account
+          </Button>
         </div>
-      </div>
-    );
-  }
-
-  function AccountView() {
-    return (
-      <div className="flex w-full justify-center">
-        <div className="flex flex-col w-full max-w-md items-center">
-          <h1 className="text-5xl font-bold pb-4">Account</h1>
-          <h3 className="text-2xl font-semibold pb-4">{user!.email}</h3>
-          <div className="flex pb-4">
-            <Button
-              className="max-w-fit"
-              variant="destructive"
-              onClick={handleLogout}
-            >
-              Logout
-            </Button>
-            <Button
-              className="max-w-fit"
-              variant="destructive"
-              onClick={() => alert("not implemented")}
-            >
-              Delete Account
-            </Button>
-          </div>
-          <div className="pb-2">
-            <ItemGroup>
-              <Item variant="outline">
-                {spotifyUser && (
-                  <ItemMedia variant="image">
-                    <a href={spotifyUser?.external_url}>
-                      <img
-                        src={spotifyUser?.image_url}
-                        alt="Profile"
-                        width={32}
-                        height={32}
-                        className="object-cover"
-                      />
-                    </a>
-                  </ItemMedia>
-                )}
-                <ItemContent>
-                  <ItemTitle>Spotify Account</ItemTitle>
-                  <ItemDescription>
-                    <a target="_blank" href={spotifyUser?.external_url}>
-                      {spotifyUser?.display_name}
-                    </a>
-                  </ItemDescription>
-                </ItemContent>
-                <ItemActions>
-                  {!spotifyUser && (
-                    <Button onClick={connectSpotifyAccount}>
-                      connect account
-                    </Button>
-                  )}
-                </ItemActions>
-              </Item>
-              <Item variant="outline">
-                <ItemContent>
-                  <ItemTitle>Apple Music Account</ItemTitle>
-                  <ItemDescription>None</ItemDescription>
-                </ItemContent>
-                <ItemActions>
-                  <Button onClick={connectAppleMusicAccount}>
+        <div className="pb-2">
+          <ItemGroup>
+            <Item variant="outline">
+              {spotifyUser && (
+                <ItemMedia variant="image">
+                  <a href={spotifyUser?.external_url}>
+                    <img
+                      src={spotifyUser?.image_url}
+                      alt="Profile"
+                      width={32}
+                      height={32}
+                      className="object-cover"
+                    />
+                  </a>
+                </ItemMedia>
+              )}
+              <ItemContent>
+                <ItemTitle>Spotify Account</ItemTitle>
+                <ItemDescription>
+                  <a target="_blank" href={spotifyUser?.external_url}>
+                    {spotifyUser?.display_name}
+                  </a>
+                </ItemDescription>
+              </ItemContent>
+              <ItemActions>
+                {!spotifyUser && (
+                  <Button onClick={connectSpotifyAccount}>
                     connect account
                   </Button>
-                </ItemActions>
-              </Item>
-            </ItemGroup>
-          </div>
+                )}
+              </ItemActions>
+            </Item>
+            <Item variant="outline">
+              <ItemContent>
+                <ItemTitle>Apple Music Account</ItemTitle>
+                <ItemDescription>None</ItemDescription>
+              </ItemContent>
+              <ItemActions>
+                <Button onClick={connectAppleMusicAccount}>
+                  connect account
+                </Button>
+              </ItemActions>
+            </Item>
+          </ItemGroup>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+function GuestView() {
+  return (
+    <div className="flex w-full justify-center">
+      <div className="flex flex-col w-full max-w-md items-center">
+        <p className="font-extrabold text-4xl">not logged in</p>
+      </div>
+    </div>
+  );
+}
+
+export default function Account() {
+  const { user } = useAuthContext();
+  const { updateSpotifyUser } = useSpotifyContext();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    if (params.get("spotify_connected") === "true") {
+      updateSpotifyUser();
+    }
+  }, [updateSpotifyUser]);
 
   /**
    * Conditional display
