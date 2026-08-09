6fd943ad236b324a995951a4ecabf2eb67e793a3
matterladd <michael.cook972@gmail.com>
2026-08-09 15:41:29 -0400

    mobile navbar improvements, move theme toggle

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -5,84 +5,110 @@ import {
   NavigationMenuItem,
   NavigationMenuLink,
   navigationMenuTriggerStyle,
+  NavigationMenuTrigger,
+  NavigationMenuContent,
 } from "@/components/ui/navigation-menu";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
-import { Button } from "./ui/button";
 import ThemeToggle from "@/components/theme-toggle";
-import { HugeiconsIcon } from "@hugeicons/react";
-import { Hamburger } from "@hugeicons/core-free-icons";
 import { useAuthContext } from "@/context/auth";
 
-const allNavLinks: { onlyGuest: boolean, onlyAuthenticated: boolean, label: string; to: string }[] = [
+const allNavLinks: {
+  onlyGuest: boolean;
+  onlyAuthenticated: boolean;
+  label: string;
+  to: string;
+}[] = [
   { onlyGuest: false, onlyAuthenticated: false, label: "Home", to: "/" },
   { onlyGuest: true, onlyAuthenticated: false, label: "Login", to: "/login" },
-  { onlyGuest: true, onlyAuthenticated: false, label: "Sign up", to: "/signup" },
-  { onlyGuest: false, onlyAuthenticated: true, label: "Account", to: "/account" },
+  {
+    onlyGuest: true,
+    onlyAuthenticated: false,
+    label: "Sign up",
+    to: "/signup",
+  },
+  {
+    onlyGuest: false,
+    onlyAuthenticated: true,
+    label: "Account",
+    to: "/account",
+  },
 ];
+
+function ListItem({
+  title,
+  children,
+  to,
+  ...props
+}: React.ComponentPropsWithoutRef<"li"> & { to: string }) {
+  return (
+    <li {...props}>
+      <NavigationMenuLink
+        closeOnClick={true}
+        render={
+          <Link to={to}>
+            <div className="flex flex-col gap-1 text-sm">
+              <div className="leading-none font-medium">{title}</div>
+              <div className="line-clamp-2 text-muted-foreground">
+                {children}
+              </div>
+            </div>
+          </Link>
+        }
+      />
+    </li>
+  );
+}
 
 export default function NavBar() {
   const { user } = useAuthContext();
   let navLinks: { label: string; to: string }[];
   if (!user) {
-    navLinks = allNavLinks.filter(link => link.onlyAuthenticated !== true);
+    navLinks = allNavLinks.filter((link) => link.onlyAuthenticated !== true);
   } else {
-    navLinks = allNavLinks.filter(link => link.onlyGuest !== true);
+    navLinks = allNavLinks.filter((link) => link.onlyGuest !== true);
   }
 
   return (
     <>
       {/* breakpoint for desktop resolutions */}
-      <NavigationMenu className={"hidden sm:flex"}>
-        <NavigationMenuList>
-          {navLinks.map((link) => (
-            <NavigationMenuItem>
-              <NavigationMenuLink
-                render={<Link to={link.to}></Link>}
-                className={navigationMenuTriggerStyle()}
-              >
-                {link.label}
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          ))}
-          {/* // TODO whole surface of button not clickable */}
-          <NavigationMenuItem className={navigationMenuTriggerStyle()}>
-            <ThemeToggle />
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+      <div className="hidden sm:flex items-center justify-between">
+        <NavigationMenu>
+          <NavigationMenuList>
+            {navLinks.map((link) => (
+              <NavigationMenuItem>
+                <NavigationMenuLink
+                  render={<Link to={link.to}></Link>}
+                  className={navigationMenuTriggerStyle()}
+                >
+                  {link.label}
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            ))}
+            {/* // TODO whole surface of button not clickable */}
+            <NavigationMenuItem
+              className={navigationMenuTriggerStyle()}
+            ></NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        <ThemeToggle className="p-4" />
+      </div>
 
       {/* breakpoint for mobile resolutions */}
-      <div className="sm:hidden">
-        <Sheet>
-          <SheetTrigger
-            render={
-              <Button variant={"ghost"}>
-                <HugeiconsIcon icon={Hamburger}></HugeiconsIcon>
-              </Button>
-            }
-          ></SheetTrigger>
-          <SheetContent
-            side="left"
-            className={"w-3/4 max-w-none sm:max-w-none min-w-0"}
-          >
-            <SheetHeader>
-              <SheetTitle>MixMover</SheetTitle>
-              <SheetDescription></SheetDescription>
-            </SheetHeader>
-            {navLinks.map((link) => (
-              <div className="p-4">
-                <Link to={link.to}>{link.label}</Link>
-              </div>
-            ))}
-          </SheetContent>
-        </Sheet>
+      <div className="sm:hidden flex items-center justify-between">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Menu</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul>
+                  {navLinks.map((link) => (
+                    <ListItem to={link.to} title={link.label}></ListItem>
+                  ))}
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        <ThemeToggle className="p-4" />
       </div>
     </>
   );
--- client/src/components/theme-toggle.tsx
+++ client/src/components/theme-toggle.tsx
@@ -2,12 +2,12 @@ import { useTheme } from "@/context/theme/index";
 import { Sun, Moon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
 
-export default function ThemeToggle() {
+export default function ThemeToggle({ className }: { className: string }) {
   const { theme, setTheme } = useTheme();
   const toggle = () =>
     theme === "dark" ? setTheme("light") : setTheme("dark");
   return (
-    <button onClick={toggle}>
+    <button className={className} onClick={toggle}>
       {theme === "dark" && <HugeiconsIcon icon={Moon} />}
       {theme === "light" && <HugeiconsIcon icon={Sun} />}
     </button>
