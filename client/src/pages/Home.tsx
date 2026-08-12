bcfe7b21e88eaeeaa3ac752a35260f548bd774bd
matterladd <michael.cook972@gmail.com>
2026-08-12 10:42:32 -0400

    add an about section

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { toast } from "sonner";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   const [appleLink, setAppleLink] = useState("");
@@ -59,8 +60,11 @@ export default function Home() {
             className="flex flex-col md:flex-row pl-4 pr-4 justify-center items-center"
             onSubmit={handleSubmit}
           >
-            <label htmlFor="appleLink" className="text-nowrap pb-2 md:pb-0">
-              Apple Music Playlist link:
+            <label
+              htmlFor="appleLink"
+              className="font-semibold text-nowrap pb-2 md:pb-0"
+            >
+              Apple Music → Spotify:
             </label>
             <div className="md:pr-2 md:pl-2 w-full md:max-w-lg md:w-lg">
               <Input
@@ -80,7 +84,7 @@ export default function Home() {
           </form>
         </Card>
       </div>
-      <div className="flex justify-center pt-10">
+      <div className="flex justify-center pt-10 pb-12">
         <Card className="w-5/6 md:max-w-6xl">
           <CardHeader>
             <CardTitle className="flex text-2xl font-bold tracking-tight">
@@ -88,7 +92,38 @@ export default function Home() {
             </CardTitle>
           </CardHeader>
           <CardContent>
-            <p>Card Content</p>
+            <p>
+              MixMover is a tool that lets you convert existing music playlists
+              on services such as <em className="font-semibold">Apple Music</em>{" "}
+              and <em className="font-semibold">Spotify</em> to your favorite
+              music streaming service. No more manual copying!
+            </p>
+          </CardContent>
+          <CardHeader>
+            <CardTitle className="flex text-2xl font-bold tracking-tight">
+              How it works:
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ol className="list-decimal list-inside marker:text-xl marker:font-semibold">
+              <li>
+                <Link to="/signup" className="font-semibold hover:underline">
+                  Create a MixMover Account
+                </Link>
+              </li>
+              <li>Connect your external account(s)</li>
+              <ul className="list-disc list-inside marker:text-xl marker:font-semibold pl-6 pb-4">
+                <li>
+                  Go to your{" "}
+                  <Link to="/account" className="font-semibold hover:underline">
+                    MixMover Account
+                  </Link>{" "}
+                  and connect the external account(s) you want to save your converted
+                  playlists to
+                </li>
+              </ul>
+              <li>Paste the playlist link above and behold!</li>
+            </ol>
           </CardContent>
         </Card>
       </div>
