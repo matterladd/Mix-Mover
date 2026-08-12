28331e83025900489cd9b6e15801e8bbe000a93d
matterladd <michael.cook972@gmail.com>
2026-08-12 09:35:51 -0400

    add a text area for app info, improve responsive styling for playlist link submission

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { toast } from "sonner";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
   const [appleLink, setAppleLink] = useState("");
@@ -49,28 +50,48 @@ export default function Home() {
       <h1 className="flex justify-center text-6xl font-bold tracking-tight">
         MixMover
       </h1>
-      <form
-        className="flex flex-col md:flex-row p-8 justify-center items-center"
-        onSubmit={handleSubmit}
-      >
-        <label htmlFor="appleLink" className="text-nowrap">
-          Apple Music Playlist link:
-        </label>
-        <div className="md:pr-2 md:pl-2 max-w-md md:max-w-lg md:w-lg">
-          <Input
-            id="appleLink"
-            className=""
-            value={appleLink}
-            onChange={(e) => setAppleLink(e.target.value)}
-            required
-          />
-        </div>
-        <div className="pt-2 md:pt-0">
-          <Button type="submit" disabled={isLoading} className="">
-            convert
-          </Button>
-        </div>
-      </form>
+      <h2 className="flex justify-center text-2xl font-semibold tracking-tight">
+        Playlist Converter App
+      </h2>
+      <div className="flex justify-center items-center pt-4">
+        <Card className="w-5/6 md:w-fit">
+          <form
+            className="flex flex-col md:flex-row pl-4 pr-4 justify-center items-center"
+            onSubmit={handleSubmit}
+          >
+            <label htmlFor="appleLink" className="text-nowrap pb-2 md:pb-0">
+              Apple Music Playlist link:
+            </label>
+            <div className="md:pr-2 md:pl-2 w-full md:max-w-lg md:w-lg">
+              <Input
+                id="appleLink"
+                className=""
+                value={appleLink}
+                placeholder="https://music.apple.com/us/playlist/example/abc"
+                onChange={(e) => setAppleLink(e.target.value)}
+                required
+              />
+            </div>
+            <div className="pt-2 md:pt-0 w-full md:w-fit">
+              <Button type="submit" disabled={isLoading} className="w-full">
+                convert
+              </Button>
+            </div>
+          </form>
+        </Card>
+      </div>
+      <div className="flex justify-center pt-10">
+        <Card className="w-5/6 md:max-w-6xl">
+          <CardHeader>
+            <CardTitle className="flex text-2xl font-bold tracking-tight">
+              What is this?
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+        </Card>
+      </div>
     </>
   );
 }
